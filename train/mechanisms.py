027bae8c2fca7b79e90324acf4679c7b692473ca
XJiaoLEE <540389496@qq.com>
2024-11-19 06:05:49 +0000

    Updated specific folders with new changes

--- train/mechanisms.py
+++ train/mechanisms.py
@@ -154,9 +154,9 @@ class MultinomialSamplingMechanism(CompressedMechanism):
         B = 2**self.budget
         range_B = np.arange(B).astype(int)
 
-        for a in z:
-            # assert np.isclose(self.P[a].sum(), 1), f"Probabilities do not sum to 1: {self.P[a]}"
-            assert len(self.P[a].shape) == 1, f"Expected self.P[{a}] to be 1-dimensional, but got shape {self.P[a].shape}"
+        # for a in z:
+        #     # assert np.isclose(self.P[a].sum(), 1), f"Probabilities do not sum to 1: {self.P[a]}"
+        #     assert len(self.P[a].shape) == 1, f"Expected self.P[{a}] to be 1-dimensional, but got shape {self.P[a].shape}"
 
         z = np.array([np.random.choice(range_B, p=self.P[a]) for a in z])
         return z
