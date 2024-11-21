dfcf03a9736582707759ab72945d357565b8f86c
XJiaoLEE <540389496@qq.com>
2024-11-21 09:53:26 +0000

    Updated specific folders with new changes

--- FL_run.py
+++ FL_run.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     epsilon = float(input("Enter the epsilon value (enter 0 if not applicable): "))
 
     # 检查输入的机制是否有效
-    valid_mechanisms = ["BASELINE", "QSGD", "PPGC", "ONEBIT", "RAPPOR", "TERNGRAD"]
+    valid_mechanisms = ["BASELINE", "QSGD", "PPGC", "ONEBIT", "RAPPOR", "TernGrad"]
     if mechanism not in valid_mechanisms:
         print("无效的机制类型。请输入以下之一: BASELINE, QSGD, PPGC, ONEBIT, RAPPOR, TernGrad")
     else:
