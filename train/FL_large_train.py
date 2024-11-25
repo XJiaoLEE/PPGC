dfd870a06724993898b2f14f1dfbc024a5bfc332
XJiaoLEE <540389496@qq.com>
2024-11-25 08:02:24 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -26,11 +26,11 @@ print(f"CUDA version: {torch.version.cuda}")
 
 # 参数设置
 NUM_ROUNDS = 200          # 联邦学习轮数
-EPOCHS_PER_CLIENT = 2    # 每轮客户端本地训练次数 4
-BATCH_SIZE = 125          # 批大小32 300 FOR MNIST 200 FOR CIFAR100 125 FOR CIFAR10
+EPOCHS_PER_CLIENT = 4    # 每轮客户端本地训练次数 4
+BATCH_SIZE = 300          # 批大小32 300 FOR MNIST 200 FOR CIFAR100 125 FOR CIFAR10
 LEARNING_RATE = 0.01    # 学习率
 epsilon = 0.0            # DP 使用的 epsilon 值
-NUM_CLIENTS_PER_NODE = 10  # 每个主机上的客户端数量125
+NUM_CLIENTS_PER_NODE = 125  # 每个主机上的客户端数量125
 
 # 检测是否有可用的 GPU，如果没有则使用 CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -271,7 +271,7 @@ def train_client(global_model, rank, world_size, client_datasets, mechanism='BAS
         for epoch in range(EPOCHS_PER_CLIENT):
             log_with_time(f"Client {args.rank * NUM_CLIENTS_PER_NODE + client_idx}, Training epoch {epoch + 1}")
             for step, (data, target) in enumerate(client_loader):
-                log_with_time(f"Client {args.rank * NUM_CLIENTS_PER_NODE + client_idx}, Training step {step + 1}")
+                # log_with_time(f"Client {args.rank * NUM_CLIENTS_PER_NODE + client_idx}, Training step {step + 1}")
                 data, target = data.to(device), target.to(device)
                 optimizer.zero_grad()
                 output = model(data)
