e3b75d949d6fdf79c16c086acad805742eb0ffac
XJiaoLEE <540389496@qq.com>
2024-12-10 03:12:09 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -22,7 +22,7 @@ print(f"Is CUDA available: {torch.cuda.is_available()}")
 print(f"CUDA version: {torch.version.cuda}")
 
 # 参数设置
-NUM_ROUNDS = 10000          # 联邦学习轮数
+NUM_ROUNDS = 3000          # 联邦学习轮数
 EPOCHS_PER_CLIENT = 1    # 每轮客户端本地训练次数 4
 BATCH_SIZE = 250          # 批大小32 300 FOR MNIST 200 FOR CIFAR100 125 FOR CIFAR10
 LEARNING_RATE = 0.01    # 学习率
