911a4e98725e72ad50e0c83ddc9d7571b12eaad1
XJiaoLEE <540389496@qq.com>
2024-12-27 10:08:33 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -439,8 +439,6 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
                     loss = criterion(output, target)
                     loss.backward()
                     optimizer.step()
-                    aggregated_accuracy = test_model(model, test_loader)
-                    log_with_time(f"Model accuracy at client {client_idx} : {aggregated_accuracy:.4f}")
                     # dist.barrier()
                     if accumulated_gradients is None:
                         accumulated_gradients = {name: torch.zeros_like(param.grad) for name, param in model.named_parameters() if param.requires_grad}
@@ -456,6 +454,7 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
                 aggregated_accuracy = test_model(model, test_loader)
                 log_with_time(f"Model accuracy at client {client_idx} : {aggregated_accuracy:.4f}")
                 scheduler.step()
+                print("optimizer.__getattribute__('param_groups')[0]['lr']",optimizer.__getattribute__('param_groups')[0]['lr'])
         # for name, param in global_model.named_parameters():
         #     if param.requires_grad:
         #         param.grad=accumulated_gradients[name] / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT)
