edb2db3a0729e5a4a04b217cae216a9c3459c9ba
XJiaoLEE <540389496@qq.com>
2024-12-30 03:29:18 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -488,13 +488,13 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
         for name, param in model.named_parameters():
             if param.requires_grad:    
                 if name == "module.layer1.0.conv2.weight":
-                    print("name before aggregation",name,accumulated_gradients[name][0]) 
-                    print("global name before aggregation",name,param.grad[0])  
+                    print("name before aggregation",name,accumulated_gradients[name][0][0]) 
+                    print("global name before aggregation",name,param.grad[0][0])  
                 dist.all_reduce(accumulated_gradients[name], op=dist.ReduceOp.SUM) 
                 param.grad=accumulated_gradients[name] / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT*args.world_size)
                 if name == "module.layer1.0.conv2.weight":
-                    print("name after aggregation",name,accumulated_gradients[name][0])   
-                    print("global name after aggregation",name,param.grad[0]) 
+                    print("name after aggregation",name,accumulated_gradients[name][0][0])   
+                    print("global name after aggregation",name,param.grad[0][0]) 
         # for name, param in global_model.named_parameters():
         #     if param.requires_grad:
         #         param.grad=accumulated_gradients[name] / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT)
