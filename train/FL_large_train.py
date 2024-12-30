bfd8f80376bbc1883e9ef5c3242d9bf0ffa254db
XJiaoLEE <540389496@qq.com>
2024-12-30 07:18:04 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -62,7 +62,7 @@ if args.dataset != 'MNIST':
     LEARNING_RATE = 0.0001
     BATCH_SIZE = 125  #125
     EPOCHS_PER_CLIENT = 10#500 //2
-    NUM_CLIENTS_PER_NODE = 5
+    NUM_CLIENTS_PER_NODE = 10
     NUM_ROUNDS = 3000
 # if args.dataset != 'MNIST':
 #     LEARNING_RATE = 0.001
@@ -443,7 +443,7 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
     for round in range(NUM_ROUNDS):
         
         log_with_time(f"Round {round + 1}/{NUM_ROUNDS} started")
-        selected_clients = random.sample(range(total_local_clients), total_local_clients // 1)  # Randomly select half of the clients
+        selected_clients = random.sample(range(total_local_clients), total_local_clients // 2)  # Randomly select half of the clients
         print("selected_clients",selected_clients)
         accumulated_gradients=None
         global_model.train()
