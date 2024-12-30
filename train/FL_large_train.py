5e5009fff5ab7b4c2800fcc8898ab52e64fa1b4e
XJiaoLEE <540389496@qq.com>
2024-12-30 09:09:09 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -498,14 +498,14 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
         for name, param in global_model.named_parameters():
             if param.requires_grad: 
                 
-                print("global paramter",param[0][0])
+                print("global paramter",param.data[0][0])
                 param.grad=accumulated_gradients[name].to(param.device) / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT*args.world_size)
                 param.data -= global_optimizer.__getattribute__('param_groups')[0]['lr'] * param.grad
                 if name == "module.layer1.0.conv2.weight":
                     print("name after aggregation",name,accumulated_gradients[name][0][0])   
                     
                     # print("global after aggregation",param.grad[0][0])   
-                    print("global paramter",param[0][0])
+                    print("global paramter",param.data[0][0])
         # for name, param in global_model.named_parameters():
         #     if param.requires_grad:
         #         param.grad=accumulated_gradients[name] / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT)
