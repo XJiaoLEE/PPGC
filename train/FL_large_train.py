bcd8964e4f555c8b9ec573952faefb5c2eab02e9
XJiaoLEE <540389496@qq.com>
2024-12-30 17:51:20 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -403,7 +403,7 @@ from torch.optim.lr_scheduler import StepLR
 # Create client models only once
 client_models = [create_model() for _ in range(NUM_CLIENTS_PER_NODE)]
 optimizers = [optim.Adam(model.parameters(), lr=LEARNING_RATE) for model in client_models]
-schedulers = [StepLR(optimizer, step_size=70, gamma=0.5) for optimizer in optimizers]
+schedulers = [StepLR(optimizer, step_size=300, gamma=0.5) for optimizer in optimizers]
 # optimizers = [torch.optim.Adam(model.parameters(), lr=LEARNING_RATE) for model in client_models]
 gradient_compressor = GradientCompressor(mechanism, sparsification_ratio, epsilon, args.out_bits)
 state = {'gradient_compressor': gradient_compressor}
@@ -414,7 +414,7 @@ for model in client_models:
 global_model = create_model()
 # 使用 Adam 作为优化器，设置合适的学习率
 global_optimizer = optim.Adam(global_model.parameters(), lr=LEARNING_RATE)
-global_scheduler = StepLR(global_optimizer, step_size=7, gamma=1)
+global_scheduler = StepLR(global_optimizer, step_size=30, gamma=1)
 # global_optimizer = optim.Adam(global_model.parameters(), lr=LEARNING_RATE)  # 你可以根据需要调整lr
 # global_optimizer = optim.SGD(global_model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
 gradient_compressor = GradientCompressor(mechanism, sparsification_ratio, epsilon, args.out_bits)
