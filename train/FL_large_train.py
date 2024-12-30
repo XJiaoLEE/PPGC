86721ce0a2007401efa88d77f3df572475ed62b1
XJiaoLEE <540389496@qq.com>
2024-12-30 09:09:52 +0000

    Updated specific folders with new changes

--- train/FL_large_train.py
+++ train/FL_large_train.py
@@ -500,7 +500,7 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
                 
                 print("global paramter",param.data[0][0])
                 param.grad=accumulated_gradients[name].to(param.device) / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT*args.world_size)
-                param.data -= global_optimizer.__getattribute__('param_groups')[0]['lr'] * param.grad
+                # param.data -= global_optimizer.__getattribute__('param_groups')[0]['lr'] * param.grad
                 if name == "module.layer1.0.conv2.weight":
                     print("name after aggregation",name,accumulated_gradients[name][0][0])   
                     
@@ -510,14 +510,14 @@ def train_epoch(global_model, global_optimizer, client_datasets, test_loader, me
         #     if param.requires_grad:
         #         param.grad=accumulated_gradients[name] / (len(selected_clients)*len(client_loader)*EPOCHS_PER_CLIENT)
         
-        # global_optimizer.step()
-        # global_scheduler.step()
+        global_optimizer.step()
+        global_scheduler.step()
         for name, param in global_model.named_parameters():
             if param.requires_grad:    
                 if name == "module.layer1.0.conv2.weight":
                     # print("global gradient paramter",name,param.grad[0][0])
 
-                    print("global paramter",name,param[0][0])
+                    print("global paramter",name,param.data[0][0])
         aggregated_accuracy = test_model(global_model, test_loader)
         log_with_time(f"Global model accuracy after aggregation: {aggregated_accuracy:.4f}")
         print("global_optimizer.__getattribute__('param_groups')[0]['lr']",global_optimizer.__getattribute__('param_groups')[0]['lr'])
